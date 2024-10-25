eba4cc8eb006c1f87568fc5d9d4c7af92765879e
MR0808 <kram@grebnesor.com>
2024-10-25 17:02:36 +1100

    More updates

---
+++ components/form/FormContainer.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+
+import { toast } from 'sonner';
+import { actionFunction } from '@/types';
+
+const initialState = {
+    result: null,
+    message: ''
+};
+
+const FormContainer = ({
+    action,
+    children,
+    sendDataToParent
+}: {
+    action: actionFunction;
+    children: React.ReactNode;
+    sendDataToParent: (data: string) => void;
+}) => {
+    const [state, formAction] = useActionState(action, initialState);
+    const { update } = useSession();
+    useEffect(() => {
+        if (state.result && state.message) {
+            update();
+            toast.success(state.message);
+            sendDataToParent('updated');
+        }
+        if (!state.result && state.message) {
+            toast.error(state.message);
+        }
+    }, [state]);
+    return <form action={formAction}>{children}</form>;
+};
+export default FormContainer;
