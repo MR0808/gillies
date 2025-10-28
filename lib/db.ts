fee4defbdcb0c50945253e5867f75cad47e05d89
MR0808 <kram@grebnesor.com>
2025-10-28 23:50:20 +1100

    Fixed auth

--- lib/db.ts
+++ lib/db.ts
@@ -1,27 +1,27 @@
-// import { PrismaClient } from '@/generated/prisma';
-
-// const prismaClientSingleton = () => {
-//     return new PrismaClient();
-// };
-
-// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
-// const globalForPrisma = globalThis as unknown as {
-//     prisma: PrismaClientSingleton | undefined;
-// };
-
-// const db = globalForPrisma.prisma ?? prismaClientSingleton();
-
-// export default db;
-
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
-
 import { PrismaClient } from '@/generated/prisma';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prismaClientSingleton = () => {
+    return new PrismaClient();
+};
 
-const db = globalForPrisma.prisma || new PrismaClient();
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClientSingleton | undefined;
+};
+
+const db = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default db;
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
+
+// import { PrismaClient } from '@/generated/prisma';
+
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// const db = globalForPrisma.prisma || new PrismaClient();
+
+// export default db;
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
