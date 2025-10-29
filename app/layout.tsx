8e2da07fff72b3d7aa1aa48a2338d20e4583d710
MR0808 <kram@grebnesor.com>
2025-10-30 09:18:58 +1100

    Lots of things removed, so so close

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default async function RootLayout({
                 <Providers>
                     {children}
                     <Toaster richColors />
-                    {/* <SpeedInsights /> */}
+                    <SpeedInsights />
                 </Providers>
             </body>
         </html>
--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -2,20 +2,13 @@
 import React from 'react';
 
 import ThemeProvider from '@/components/dashboardLayout/ThemeToggle/ThemeProvider';
-import { QueryProvider } from './QueryProvider';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
-            <QueryProvider>
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="system"
-                    enableSystem
-                >
-                    {children}
-                </ThemeProvider>
-            </QueryProvider>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+                {children}
+            </ThemeProvider>
         </>
     );
 };
