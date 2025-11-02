4e08e97c680b7113d98823ca12e19b9faef29895
MR0808 <kram@grebnesor.com>
2025-11-03 09:52:44 +1100

    big test of things

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,30 @@
+// import { PrismaClient } from '@/generated/prisma';
+
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// const db = globalForPrisma.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
+
+// export default db;
+
 import { PrismaClient } from '@/generated/prisma';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// Use `any` on the global cache to avoid type-narrowing conflicts with the extended client
+const globalForPrisma = globalThis as unknown as {
+    prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
 
-const db = globalForPrisma.prisma || new PrismaClient();
+function createPrismaClient() {
+    const client = new PrismaClient({
+        datasourceUrl: process.env.DATABASE_URL // prisma://accelerate... URL
+    }).$extends(withAccelerate());
+
+    return client;
+}
+
+export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
 
